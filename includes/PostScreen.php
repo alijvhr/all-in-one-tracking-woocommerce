d2562ca023b3dd199becc91636bdbd5b50314514
Alijvhr <eng.alijvhr@gmail.com>
2023-09-20 14:15:29 +0330

    — save and retrieve data — translation

--- includes/PostScreen.php
+++ includes/PostScreen.php
@@ -4,19 +4,21 @@ namespace AllInOneTrackingWoocommerce\includes;
 
 class PostScreen {
 
-	public function init(){
+	public function init() {
 		add_action( 'load-post.php', [ $this, 'load' ] );
 		add_action( 'load-post-new.php', [ $this, 'load' ] );
-		add_action('admin_enqueue_scripts', [$this, 'meta_box_style' ]);
-	}
-	public function load() {
-		add_action( 'add_meta_boxes', [ $this, 'add_meta_box' ] );
+		add_action( 'admin_enqueue_scripts', [ $this, 'meta_box_style' ] );
+		add_action( 'save_post', [ $this, 'save' ] );
 	}
 
-	public function add_meta_box() {
+	public function load() {
+		add_action( 'add_meta_boxes_shop_order', [ $this, 'add_meta_box' ]);
+	}
+
+	public function add_meta_box(\WP_Post $post) {
 		add_meta_box(
 			'wootaio-order-tracking',
-			esc_html__( 'Order Tracking', 'example' ),
+			__( 'Order Tracking', 'all-in-one-tracking-woocommerce' ),
 			[ $this, 'render_meta_box' ],
 			'shop_order',
 			'side',
@@ -24,15 +26,43 @@ class PostScreen {
 		);
 	}
 
-	public function render_meta_box() {
-		include WOOTAIO_PLUGIN_PATH."/views/meta_box.php";
+	public function save( $post_id ) {
+
+		if ( ! wp_verify_nonce( $_POST['wootaio_nonce'] ?? '', 'wootaio' ) ) {
+			return;
+		}
+
+		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+			return;
+		}
+
+		if ( ! current_user_can( 'edit_post', $post_id ) ) {
+			return;
+		}
+
+		$title    = sanitize_text_field( $_POST['wootaio_tracking_title'] );
+		$code     = sanitize_text_field( $_POST['wootaio_tracking_code'] );
+		$link     = sanitize_text_field( $_POST['wootaio_tracking_link'] );
+		$link     = str_replace( '{code}', $code, $link );
+		$duration = sanitize_text_field( $_POST['wootaio_tracking_duration'] );
+
+		update_post_meta( $post_id, 'wootaio_tracking_title', $title );
+		update_post_meta( $post_id, 'wootaio_tracking_code', $code );
+		update_post_meta( $post_id, 'wootaio_tracking_link', $link );
+		update_post_meta( $post_id, 'wootaio_tracking_duration', $duration );
 	}
 
 
-	public function meta_box_style()
-	{
-		wp_register_style('wootaio_css_script', plugin_dir_url(__FILE__) . '../assets/css/admin.css', false, '1.2');
-		wp_enqueue_style('wootaio_css_script');
+	public function render_meta_box() {
+		wp_nonce_field( 'wootaio', 'wootaio_nonce' );
+		$meta = get_post_meta(get_the_ID());
+		include WOOTAIO_PLUGIN_PATH . "/views/meta_box.php";
+	}
+
+
+	public function meta_box_style() {
+		wp_register_style( 'wootaio_css_script', plugin_dir_url( __FILE__ ) . '../assets/css/admin.css', false, '1.2' );
+		wp_enqueue_style( 'wootaio_css_script' );
 	}
 
 }
--- views/meta_box.php
+++ views/meta_box.php
@@ -1,26 +1,27 @@
 <ul class="wootaio-order-tracking">
     <li>
         <label class="wootaio-full-width">
-            <span>Shipment Title:</span>
-            <input type="text" name="wootaio-tracking-title">
+            <span><?= __( 'Shipment Title', 'all-in-one-tracking-woocommerce' ) ?>:</span>
+            <input type="text" name="wootaio_tracking_title"
+                   value="<?= $meta['wootaio_tracking_title'][0] ?? __( 'post', 'all-in-one-tracking-woocommerce' ) ?>">
         </label>
     </li>
     <li>
         <label class="wootaio-full-width">
-            <span>Tracking Code:</span>
-            <input type="text" name="wootaio-tracking-code">
+            <span><?= __( 'Tracking Number', 'all-in-one-tracking-woocommerce' ) ?>:</span>
+            <input type="text" name="wootaio_tracking_code" value="<?= $meta['wootaio_tracking_code'][0] ?? '' ?>">
         </label>
     </li>
     <li>
         <label class="wootaio-full-width">
-            <span>Tracking Link:</span>
-            <input type="text" name="wootaio-tracking-link">
+            <span><?= __( 'Tracking Link', 'all-in-one-tracking-woocommerce' ) ?>:</span>
+            <input type="text" name="wootaio_tracking_link" value="<?= $meta['wootaio_tracking_link'][0] ?? '' ?>">
         </label>
     </li>
     <li>
         <label class="wootaio-full-width">
-            <span>Shipment Duration:</span>
-            <input type="text" name="wootaio-tracking-duration">
+            <span><?= __( 'Shipment Duration', 'all-in-one-tracking-woocommerce' ) ?>:</span>
+            <input type="text" name="wootaio_tracking_duration" value="<?= $meta['wootaio_tracking_duration'][0] ?? '' ?>">
         </label>
     </li>
 </ul>
